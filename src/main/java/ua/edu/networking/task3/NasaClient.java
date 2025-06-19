159c558f0090e29b8060091809efc498c7428615
Danylo Shlapak <d.shlapak2314@gmail.com>
2025-06-19 23:37:19 +0300

    [Task-3] Rewrite solution with RestTemplate

--- src/main/java/ua/edu/networking/task3/NasaClient.java
+++ src/main/java/ua/edu/networking/task3/NasaClient.java
@@ -3,33 +3,25 @@ package ua.edu.networking.task3;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 public class NasaClient {
     private static final String NASA_API_URL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=300&api_key=";
     private final ObjectMapper objectMapper;
-    private final HttpClient httpClient;
+    private final RestTemplate restTemplate;
     private final String apiKey;
 
     public NasaClient(String apiKey) {
         this.apiKey = apiKey;
         this.objectMapper = new ObjectMapper();
-        this.httpClient = HttpClient.newBuilder()
-                .followRedirects(HttpClient.Redirect.ALWAYS)
-                .build();
+        this.restTemplate = new RestTemplate();
     }
 
     public void findLargestPhoto() throws Exception {
-        HttpRequest request = buildGetRequest(NASA_API_URL + apiKey);
-
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        JsonNode rootNode = objectMapper.readTree(response.body());
+        ResponseEntity<String> response = restTemplate.getForEntity(NASA_API_URL + apiKey, String.class);
+        JsonNode rootNode = objectMapper.readTree(response.getBody());
 
         String largestPhotoUrl = findLargestPhoto(rootNode);
         log.info("Largest photo URL: {}", largestPhotoUrl);
@@ -52,25 +44,10 @@ public class NasaClient {
     }
 
     private int getPhotoSize(String photoUrl) {
-        try {
-            HttpRequest request = buildGetRequest(photoUrl);
-
-            HttpResponse<Void> response = httpClient.send(request, HttpResponse.BodyHandlers.discarding());
-
-            return response.headers().firstValue("Content-Length").map(Integer::parseInt).orElse(0);
-        } catch (IOException ex) {
-            log.error("Error checking size for {}: {}", photoUrl, ex.getMessage(), ex);
-            return 0;
-        } catch (InterruptedException ex) {
-            log.error("Thread was interrupted", ex);
-            throw new RuntimeException(ex);
+        ResponseEntity<String> response = restTemplate.getForEntity(photoUrl, String.class);
+        if (response.getStatusCode().is3xxRedirection()) {
+            return getPhotoSize(response.getHeaders().getLocation().toString());
         }
-    }
-
-    private HttpRequest buildGetRequest(String url) {
-        return HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .GET()
-                .build();
+        return (int) response.getHeaders().getContentLength();
     }
 }
